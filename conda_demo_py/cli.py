22ea352a89815b0c9f6fb95dd9722677cedf9a49
Walker Hale IV <walker.hale.iv@gmail.com>
2019-02-15 15:45:58 -0600

    Use argparse

--- conda_demo_py/cli.py
+++ conda_demo_py/cli.py
@@ -1,8 +1,16 @@
 """Console scripts."""
 
+from argparse import ArgumentParser
+
+from .version import __version__
+
 
 def cdemopy():
     """All CLI entry point functions must take no arguments and return either
     None or an exit code."""
+    parser = ArgumentParser(description=cdemopy.__doc__)
+    parser.add_argument('--version', action='version',
+                        version=f'%(prog)s {__version__}')
+    args = parser.parse_args()
     print('hello from conda_demo_py')
     # No return value -> None -> no error
